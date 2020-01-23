f31bc7b2bea7d2ae3da0c297cf21c6cdaf5f112a
Simon Peier <simon.peier@comerge.net>
2020-01-23 14:07:28 +0100

    Add dataclasses Airplane and Card

---
+++ src/main/java/Airplane.java
@@ -0,0 +1,33 @@
+public class Airplane {
+    private String color;
+    private boolean isBack;
+
+    public Airplane(String color, boolean isBack) {
+        this.color = color;
+        this.isBack = isBack;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isBack() {
+        return isBack;
+    }
+
+    public void setBack(boolean back) {
+        isBack = back;
+    }
+
+    @Override
+    public String toString() {
+        return "Airplane{" +
+                "color='" + color +
+                ", isBack=" + isBack +
+                '}';
+    }
+}
---
+++ src/main/java/Card.java
@@ -0,0 +1,59 @@
+public class Card {
+    private Airplane top;
+    private Airplane right;
+    private Airplane bottom;
+    private Airplane left;
+
+    public Card(Airplane top, Airplane right, Airplane bottom, Airplane left) {
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+        this.left = left;
+    }
+
+    public Airplane getTop() {
+        return top;
+    }
+
+    public void setTop(Airplane top) {
+        this.top = top;
+    }
+
+    public Airplane getRight() {
+        return right;
+    }
+
+    public void setRight(Airplane right) {
+        this.right = right;
+    }
+
+    public Airplane getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(Airplane bottom) {
+        this.bottom = bottom;
+    }
+
+    public Airplane getLeft() {
+        return left;
+    }
+
+    public void setLeft(Airplane left) {
+        this.left = left;
+    }
+
+    public Card rotated90DegreesToRight() {
+        return new Card(this.left, this.top, this.right, this.bottom);
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "top=" + top +
+                ", right=" + right +
+                ", bottom=" + bottom +
+                ", left=" + left +
+                '}';
+    }
+}
